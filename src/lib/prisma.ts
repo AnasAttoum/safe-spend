8bce795ecbaeed81e6d2f4216bd2336573f85f6e
Anas Attoum <anasattoum.12321@gmail.com>
2025-04-20 18:24:41 +0300

    Fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,9 +3,6 @@ import { PrismaClient } from '@/generated/prisma';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
