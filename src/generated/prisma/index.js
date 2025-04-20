a091fe54b4530f3a0585dafcaa9e336b88f464ad
Anas Attoum <anasattoum.12321@gmail.com>
2025-04-20 11:14:01 +0300

    try

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -84,6 +84,9 @@ Prisma.NullTypes = {
  * Enums
  */
 exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
   Serializable: 'Serializable'
 });
 
@@ -135,6 +138,11 @@ exports.Prisma.SortOrder = {
   desc: 'desc'
 };
 
+exports.Prisma.QueryMode = {
+  default: 'default',
+  insensitive: 'insensitive'
+};
+
 
 exports.Prisma.ModelName = {
   User: 'User',
@@ -172,7 +180,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -181,18 +189,18 @@ const config = {
   "datasourceNames": [
     "db"
   ],
-  "activeProvider": "sqlite",
+  "activeProvider": "postgresql",
   "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
-        "fromEnvVar": null,
-        "value": "file:./dev.db"
+        "fromEnvVar": "DATABASE_URL",
+        "value": null
       }
     }
   },
-  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client-js\"\n  output   = \"../src/generated/prisma\"\n}\n\ndatasource db {\n  provider = \"sqlite\"\n  url      = \"file:./dev.db\"\n  // provider = \"postgresql\"\n  // url      = env(\"DATABASE_URL\")\n}\n\nmodel User {\n  userId   String @id\n  currency String\n}\n\nmodel Category {\n  userId String\n\n  name String\n  icon String\n  type String @default(\"income\")\n\n  createdAt DateTime @default(now())\n\n  @@unique([name, userId, type])\n}\n\nmodel Transation {\n  id     String @id @default(uuid()) // To make prisma generate it\n  userId String\n\n  amount      Float\n  description String\n  type        String @default(\"income\")\n\n  category     String\n  categoryIcon String\n\n  date      DateTime\n  createdAt DateTime @default(now())\n  updatedAt DateTime @default(now())\n}\n\nmodel MonthTable {\n  userId String\n\n  day   Int\n  month Int\n  year  Int\n\n  income  Float\n  expense Float\n\n  @@id([day, month, year, userId])\n}\n\nmodel YearTable {\n  userId String\n\n  month Int\n  year  Int\n\n  income  Float\n  expense Float\n\n  @@id([month, year, userId])\n}\n",
-  "inlineSchemaHash": "5a2685ca18b01366960cfb1b17ddeb2cabba030e8a9ff2755bf8b7db356ee118",
+  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client-js\"\n  output   = \"../src/generated/prisma\"\n}\n\ndatasource db {\n  provider  = \"postgresql\"\n  url       = env(\"DATABASE_URL\")\n  directUrl = env(\"POSTGRES_URL_NON_POOLING\")\n  // provider = \"sqlite\"\n  // url      = \"file:./dev.db\"\n}\n\nmodel User {\n  userId   String @id\n  currency String\n}\n\nmodel Category {\n  userId String\n\n  name String\n  icon String\n  type String @default(\"income\")\n\n  createdAt DateTime @default(now())\n\n  @@unique([name, userId, type])\n}\n\nmodel Transation {\n  id     String @id @default(uuid()) // To make prisma generate it\n  userId String\n\n  amount      Float\n  description String\n  type        String @default(\"income\")\n\n  category     String\n  categoryIcon String\n\n  date      DateTime\n  createdAt DateTime @default(now())\n  updatedAt DateTime @default(now())\n}\n\nmodel MonthTable {\n  userId String\n\n  day   Int\n  month Int\n  year  Int\n\n  income  Float\n  expense Float\n\n  @@id([day, month, year, userId])\n}\n\nmodel YearTable {\n  userId String\n\n  month Int\n  year  Int\n\n  income  Float\n  expense Float\n\n  @@id([month, year, userId])\n}\n",
+  "inlineSchemaHash": "113c4cbd71fccb6c6ac4d06f4cda303bc9b316d85f69b4542c6ffc8dc84c90b4",
   "copyEngine": true
 }
 
