f854777cb91ab07712581a38f6ee0be4ad38137b
Anas Atoum <anas.atoum@darrebni.net>
2025-06-01 19:57:22 +0300

    Fix unlogged user

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,10 +1,21 @@
 import { Metadata } from "next";
 import Transactions from "@/sections/transactions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { routes } from "@/config/routes";
+import { prisma } from "@/lib/prisma";
 
 export const metadata: Metadata = {
   title: "Transactions",
 };
 
-export default function page() {
+export default async function page() {
+
+  const user = await currentUser();
+  if (!user) redirect(routes.signIn);
+
+  const userData = await prisma.user.findUnique({ where: { userId: user.id } });
+  if (!userData) redirect(routes.currency);
+
   return <Transactions />;
 }
