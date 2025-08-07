9cbf0109036039971e70959a3f5a5fbfb51aa7b5
Anas Atoum <anas.atoum@darrebni.net>
2025-08-07 07:33:08 +0300

    Add runtimeCaching to next-pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const runtimeCaching = require('next-pwa/cache');
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
 });
 
 const nextConfig: NextConfig = {
