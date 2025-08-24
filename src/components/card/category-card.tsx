843a738bf1b8ba0eaec6864e2fb4cb3eb7240bfe
Anas Atoum <anas.atoum@darrebni.net>
2025-08-24 17:17:58 +0300

    Add simple transition to categories cards

--- src/components/card/category-card.tsx
+++ src/components/card/category-card.tsx
@@ -8,11 +8,11 @@ type Props = {
 export default function CategoryCard({ category }: Props) {
   const { id = "", icon = "", name = "" } = category;
   return (
-    <Link href={{ pathname: '/', query: { categoryId: id } }} className="relative cardBg text-black rounded-sm">
+    <Link href={{ pathname: '/', query: { categoryId: id } }} className="relative cardBg rounded-sm transition-all duration-300 group hover:scale-101 hover:-translate-y-1">
       <WaveIcon className="absolute -left-10 w-full rotate-90" size={100} />
       <div className="flex items-center gap-3 p-5 text-safeSpend-primary font-bold">
-        <span className="text-6xl shrink-0">{icon}</span>
-        <span className="truncate max-w-[150px]">{name}</span>
+        <span className="text-6xl shrink-0 transition-all duration-300 group-hover:scale-105 group-hover:rotate-12">{icon}</span>
+        <span className="truncate w-full">{name}</span>
       </div>
     </Link>
   );
