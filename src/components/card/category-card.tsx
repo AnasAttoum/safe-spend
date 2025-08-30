e73cfe9c84ade1e32201984701c52d40b09fc9b2
Anas Attoum <AnasAttoum.12321@gmail.com>
2025-08-30 18:19:33 +0300

    Fix category style

--- src/components/card/category-card.tsx
+++ src/components/card/category-card.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function CategoryCard({ category }: Props) {
   const { id = "", icon = "", name = "" } = category;
   return (
-    <Link href={{ pathname: '/', query: { categoryId: id } }} className="relative cardBg rounded-sm transition-all duration-300 group hover:scale-101 hover:-translate-y-1">
+    <Link href={{ pathname: '/', query: { categoryId: id } }} scroll={true} className="relative cardBg rounded-sm transition-all duration-300 group hover:scale-101 hover:-translate-y-1">
       <WaveIcon className="absolute -left-10 w-full rotate-90" size={100} />
       <div className="flex items-center gap-3 p-5 text-safeSpend-primary font-bold">
         <span className="text-6xl shrink-0 transition-all duration-300 group-hover:scale-105 group-hover:rotate-12">{icon}</span>
--- src/sections/dashboard/index.tsx
+++ src/sections/dashboard/index.tsx
@@ -11,6 +11,7 @@ import AllTimeHistory from "./views/all-time-history";
 import { DeleteDialog } from "@/components/dialog/delete-dialog";
 import { Button } from "@/components/ui/button";
 import { Category } from "@/generated/prisma";
+import { cn } from "@/lib/utils";
 
 export type CategoryOverview = {
   category: Category;
@@ -34,7 +35,9 @@ export default async function Dashboard({ categoryOverview }: { categoryOverview
       {categoryOverview && categoryOverview.category && <div className="flex gap-3 items-center p-5 w-full">
         <span className="text-7xl" >{categoryOverview.category.icon}</span>
         <div className="flex flex-col w-full truncate">
-          <p className="text-safeSpend-primary font-bold w-full truncate">{categoryOverview.category.name}</p>
+          <p className={cn("font-bold w-full truncate",
+            categoryOverview.category.type === 'income' ? 'text-income' : 'text-expense'
+          )}>{categoryOverview.category.name}</p>
           <small className="text-gray-500">({categoryOverview.transactionsCount} Transaction{categoryOverview.transactionsCount > 1 && "s"})</small>
         </div>
       </div>}
