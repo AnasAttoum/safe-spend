e550b01c67cbc6b666fa1162cf161aaa57bb9541
Anas Attoum <AnasAttoum.12321@gmail.com>
2025-09-23 20:09:00 +0300

    Redesign categories statistics section

--- src/components/card/category-statistic.tsx
+++ src/components/card/category-statistic.tsx
@@ -1,9 +1,9 @@
 import { Categoriestype } from "@/app/api/statistics/category/route";
 import { Card } from "../ui/card";
 import { Progress } from "../ui/progress";
-import { ScrollArea } from "../ui/scroll-area";
-import { currencies } from "@/config/currencies";
+import { currencies, defaultCurrency } from "@/config/currencies";
 import CountUp from "react-countup";
+import Link from "next/link";
 
 type Props = {
   type: "income" | "expense";
@@ -14,59 +14,59 @@ export default function CategoryStatistic({ type, data }: Props) {
   const filteredData = data.filter((el) => el.type === type);
 
   return (
-    <Card className="h-60 w-full p-5">
-      <h3 className={`text-${type} text-lg font-bold`}>
+    <Card className="w-full py-5">
+      <h3 className={`text-${type} text-lg font-bold px-5`}>
         {type.charAt(0).toUpperCase() + type.slice(1)}s by category:
       </h3>
       {!filteredData.length ? (
-        <div className="flex flex-col justify-center items-center h-full">
+        <div className="flex flex-col justify-center items-center h-full px-5">
           <div>No data for the selected period</div>
           <div className="text-gray-500">
             Try selecting a different period or try adding new {type + "s"}
           </div>
         </div>
       ) : (
-        <ScrollArea className="h-36 px-4">
-          <div className="flex flex-col gap-4">
-            {filteredData.map((el, index) => {
-              const amount = el._sum.amount || 0;
-              const total = filteredData
-                .filter((data) => data.currency === el.currency)
-                .reduce((acc, el) => acc + (el._sum.amount || 0), 0);
-              const symbol = currencies.find((curr) => curr.value === el.currency)?.symbol || ''
-              const percentage = (amount * 100) / (total || amount);
-              return (
-                <div
-                  key={index}
-                  className="flex flex-col gap-2"
-                >
-                  <div className="flex justify-between">
-                    <div className="flex items-center gap-2">
-                      {el.category?.icon} {el.category?.name}
-                      <span className="bg-safeSpend-light text-white rounded-md text-xs px-1">
-                        {percentage.toFixed(1)}% {el.currency}
-                      </span>
-                    </div>
+        <div className="flex flex-col">
+          {filteredData.map((el, index) => {
+            const amount = el._sum.amount || 0;
+            const total = filteredData
+              .filter((data) => data.currency === el.currency)
+              .reduce((acc, el) => acc + (el._sum.amount || 0), 0);
+            const { symbol, color } = currencies.find((curr) => curr.value === el.currency) || defaultCurrency;
+            const percentage = (amount * 100) / (total || amount);
 
-                    <div>
-                      <CountUp end={amount} decimals={Number.isInteger(amount) ? 0 : 2} suffix={` ${symbol}`} />
-                      {/* {amount} {symbol} */}
-                    </div>
+            return (
+              <Link
+                key={index}
+                href={{ pathname: '/', query: { categoryId: el.categoryId } }}
+                className="flex flex-col gap-2 p-4 hover:bg-gray-100 dark:hover:bg-neutral-950"
+              >
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-2">
+                    {el.category?.icon} {el.category?.name}
+                    <span className={"text-white rounded-md text-xs px-1"} style={{ backgroundColor: color }}>
+                      {percentage.toFixed(1)}% {el.currency}
+                    </span>
                   </div>
 
-                  <Progress
-                    value={percentage}
-                    className={
-                      type === "income"
-                        ? "[&>div]:bg-income"
-                        : "[&>div]:bg-expense"
-                    }
-                  />
+                  <div>
+                    <CountUp end={amount} decimals={Number.isInteger(amount) ? 0 : 2} suffix={` ${symbol}`} />
+                    {/* {amount} {symbol} */}
+                  </div>
                 </div>
-              );
-            })}
-          </div>
-        </ScrollArea>
+
+                <Progress
+                  value={percentage}
+                  className={
+                    type === "income"
+                      ? "[&>div]:bg-income"
+                      : "[&>div]:bg-expense"
+                  }
+                />
+              </Link>
+            );
+          })}
+        </div>
       )}
     </Card>
   );
