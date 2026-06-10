419335a2297c75b143100ecd397980a2c84d9a08
Anas Attoum <AnasAttoum.12321@gmail.com>
2026-06-10 20:12:12 +0200

    Change grid when there is only one currency

--- src/sections/dashboard/components/total-balance-cards.tsx
+++ src/sections/dashboard/components/total-balance-cards.tsx
@@ -1,6 +1,7 @@
 import CardBalanceTotal from "@/components/card/card-balance-total";
 import SkeletonWrapper from "@/components/skeleton/skeleton";
 import { queryKey } from "@/config/query-key";
+import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 
 export default function TotalBalanceCards() {
@@ -15,7 +16,7 @@ export default function TotalBalanceCards() {
   return (
     <div className="w-full">
       <SkeletonWrapper isLoading={isLoading}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className={cn("grid grid-cols-1 gap-5", data && Array.isArray(data) && data.length > 1 && "md:grid-cols-2")}>
           {isLoading && <CardBalanceTotal balance={{ currency: 'USD', total: 0, diff: 0 }} />}
           {data && Array.isArray(data) && data?.filter((balance) => !!balance.total || !!balance.diff).map((balance: { currency: string; total: number; diff: number }, index: number) => <CardBalanceTotal key={index} balance={balance} />)}
           {data && !Array.isArray(data) && <CardBalanceTotal balance={data} />}
