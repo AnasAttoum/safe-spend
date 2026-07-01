2a8cc8dc30fc1e271c2000d79b01872e4c7c24e1
Anas Attoum <anas.attoum@icloud.com>
2026-07-01 21:34:17 +0200

    Make total card fullwidth, when there is only one currency

--- src/sections/dashboard/components/total-balance-cards.tsx
+++ src/sections/dashboard/components/total-balance-cards.tsx
@@ -13,12 +13,14 @@ export default function TotalBalanceCards() {
       ),
   });
 
+  const visibleData = data?.filter((balance: { currency: string; total: number; diff: number }) => !!balance.total || !!balance.diff);
+
   return (
     <div className="w-full">
       <SkeletonWrapper isLoading={isLoading}>
-        <div className={cn("grid grid-cols-1 gap-5", data && Array.isArray(data) && data.length > 1 && "md:grid-cols-2")}>
+        <div className={cn("grid grid-cols-1 gap-5", data && Array.isArray(data) && visibleData.length > 1 && "md:grid-cols-2")}>
           {isLoading && <CardBalanceTotal balance={{ currency: 'USD', total: 0, diff: 0 }} />}
-          {data && Array.isArray(data) && data?.filter((balance) => !!balance.total || !!balance.diff).map((balance: { currency: string; total: number; diff: number }, index: number) => <CardBalanceTotal key={index} balance={balance} />)}
+          {data && Array.isArray(data) && visibleData.map((balance: { currency: string; total: number; diff: number }, index: number) => <CardBalanceTotal key={index} balance={balance} />)}
           {data && !Array.isArray(data) && <CardBalanceTotal balance={data} />}
         </div>
       </SkeletonWrapper>
