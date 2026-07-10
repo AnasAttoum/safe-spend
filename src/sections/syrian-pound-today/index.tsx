48fa077bcae545ca98a15818dd6986e7666eecae
Anas Attoum <anas.attoum@icloud.com>
2026-07-10 22:53:43 +0200

    Continue tracking 2

--- src/sections/syrian-pound-today/index.tsx
+++ src/sections/syrian-pound-today/index.tsx
@@ -25,27 +25,11 @@ export default async function SyrianPoundToday() {
   const response = await fetch(
     process.env.NEXT_PUBLIC_SYRIAN_POUND_TODAY!, {
     headers: {
-      Accept: "application/json",
-      "User-Agent": "Mozilla/5.0",
-    },
-  })
-  console.log({
-    status: response.status,
-    url: response.url,
-    contentType: response.headers.get("content-type"),
-  });
-
-  const body = await response.text();
-
-  console.log(body);
-
-  if (!response.headers.get("content-type")?.includes("application/json")) {
-    throw new Error("API returned HTML instead of JSON");
-  }
-
-  const parsedResponse = JSON.parse(body);
-
-  const res: ServerResponse = parsedResponse?.data?.currencies || []
+      accept: 'application/json',
+      'User-agent': 'learning app',
+    }
+  }).then(res => { console.log("res: ", res); console.log("res.json(): ", res.json()); return res.json() }).catch((error) => console.error('Error in SYRIAN POUND TODAY Page', error));
+  const res: ServerResponse = response?.data?.currencies || []
   const currencies: CurrencyToday[] = Object.entries(res)
     .filter(([key]) => key.endsWith(":damascus"))
     .map(([key, value]) => {
